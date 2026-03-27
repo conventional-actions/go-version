7a88b5d301b68f1ab0215be04c7726cf6728a64c
Anton Winter <838833+antonwinter@users.noreply.github.com>
2026-03-26 22:27:55 -0700

    feat!: Migrate to ESM, esbuild, and Node 24 (#96)

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,15 +1,18 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
+import {fileURLToPath} from 'url'
 import {test} from '@jest/globals'
 
-// shows how the runner will run a javascript action with env / stdout protocol
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 test('test runs', () => {
   process.env['INPUT_VERSION'] = '0.0.0'
   const np = process.execPath
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const ip = path.join(__dirname, '..', 'dist', 'index.cjs')
   const options: cp.ExecFileSyncOptions = {
-    env: process.env
+    env: process.env,
+    cwd: path.join(__dirname, '..')
   }
 
   cp.execFileSync(np, [ip], options)
